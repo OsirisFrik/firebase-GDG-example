7c3cc5402237f8adf8a74b56a13bfe2b34bd29d5
OsirisFrik <osiris.lpz96@gmail.com>
2017-10-26 22:06:30 -0700

    delete posts

--- app/js/post.js
+++ app/js/post.js
@@ -41,20 +41,28 @@ function createPost(post, url) {
     document.getElementById('nePost').className = 'waves-effect waves-light btn right';
   });
 }
-
+var i = 0;
 function getPosts(data) {
   var postKey = data.key;
   var post = data.val();
   var html;
+  var option;
+  var menu = '';
   post.post = post.post.replace(/\r?\n/g, '<br />');
+  if (post.userId == userData.uid) {
+    menu = '<a data-activates="more-post' + postKey + '" id="menu' + postKey + '" class="dropdown-button" href="#"><i class="material-icons right" id="more-post">more_vert</i></a>';
+    option = '<ul id="more-post' + postKey + '" class="dropdown-content options"><li><a onclick="deletePost(\''+postKey+'\')"><i class="material-icons">delete</i>&nbsp; Delete</a></li></ul>';
+    document.getElementById('op').innerHTML += option;
+  }
+
   if (post.image) {
-    html = '<div class="col s12 l8 offset-l2" id="' + postKey + '">' + '<div class="card">' + '<div class="row user-post">' + '<img src="' + post.userPhoto + '" class="avatar-post circle">' + '<p class="userName-post">' + post.userName + '</p>' + '</div>' + '<div class="row post">' + '<p class="card-content post-content">' + post.post + '</p>' + '<img src="' + post.image + '" class="post-img">' + '</div>' + '</div>';
+    html = '<div class="col s12 l8 offset-l2" id="' + postKey + '"><div class="card"><div class="row user-post"><img src="' + post.userPhoto + '" class="avatar-post circle"><p class="userName-post"><a href="/profile.html#'+post.userName+'">' + post.userName + '</a></p>'+menu+'</div><div class="row post"><p class="card-content post-content">' + post.post + '</p>' + '<img src="' + post.image + '" class="post-img"></div></div>';
   } else {
-    html = '<div class="col s12 l8 offset-l2" id="' + postKey + '">' + '<div class="card">' + '<div class="row user-post">' + '<img src="' + post.userPhoto + '" class="avatar-post circle">' + '<p class="userName-post">' + post.userName + '</p>' + '</div>' + '<div class="row post">' + '<p class="card-content post-content">' + post.post + '</p>' + '</div>' + '</div>';
+    html = '<div class="col s12 l8 offset-l2" id="' + postKey + '"><div class="card"><div class="row user-post"><img src="' + post.userPhoto + '" class="avatar-post circle"><p class="userName-post"><a href="/profile.html#'+post.userName+'">' + post.userName + '</a></p>'+menu+'</div><div class="row post"><p class="card-content post-content">' + post.post + '</p></div></div>';
   }
 
   if (lastPost[0]) {
-    var last = document.getElementById(lastPost[lastPost.length-1]);
+    var last = document.getElementById(lastPost[lastPost.length - 1]);
     var parent = last.parentNode;
     var helper = document.createElement('span');
     helper.innerHTML = html;
@@ -63,7 +71,11 @@ function getPosts(data) {
     document.getElementById('posts').innerHTML = html;
   }
   lastPost.push(postKey);
-  console.log(lastPost);
+  var id = '#menu'+postKey;
+  $(id).dropdown({
+    belowOrigin: true
+  });
+  //document.getElementById('op').innerHTML += option;
 }
 
 function removePost(data) {
@@ -74,4 +86,17 @@ function removePost(data) {
     console.log(lastPost);
   }
   document.getElementById(data.key).remove();
+}
+
+function deletePost(postKey) {
+  database.ref('post/'+postKey).remove().then(function () {
+    database.ref('users/'+userData.uid).once('value', function (data) {
+      data = data.val();
+      for (var key in data.posts) {
+        if (data.posts[key] == postKey) {
+          database.ref('users/'+userData.uid+'/posts/'+key).remove();
+        }
+      }
+    });
+  });
 }
